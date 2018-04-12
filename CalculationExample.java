910c3ee3396f028697951198a85256c5c945cdfd
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-12 11:27:03 +0700

    pf-java exception number format

--- CalculationExample.java
+++ CalculationExample.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class CalculationExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number x: ");
+        int x = scanner.nextInt();
+        System.out.println(" Enter number y: ");
+        int y = scanner.nextInt();
+        CalculationExample calculationExample = new CalculationExample();
+        calculationExample.calculate(x,y);
+    }
+
+    public void calculate(int x, int y) {
+        try {
+            int a = x + y;
+            int b = x - y;
+            int c = x * y;
+            int d = x / y;
+            System.out.println("Display Sum:" + a);
+            System.out.println("Display Minus:" + b);
+            System.out.println("Display Multip:" + c);
+            System.out.println("Display division:" + d);
+        } catch (Exception e) {
+            System.out.println("Exception:" + e.getMessage());
+        }
+    }
 }
